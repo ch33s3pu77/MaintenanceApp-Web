8081087bda55eb848115ac94381e68d0c6a78f2e
Stewart Rutledge <stewart.rut@gmail.com>
2020-05-14 22:59:18 +0100

    Database connection update

--- auth/db.php
+++ auth/db.php
@@ -2,7 +2,7 @@
 define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_PASS","MadMax66");
-define("DB_NAME","myblog_db");
+define("DB_NAME","maintenanceapp");
 
 $con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
